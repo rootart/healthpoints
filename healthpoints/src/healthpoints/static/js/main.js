0c9265b41a96274f5c3490607ff610c14e9aece2
Julia Scherbina <julia.shcherbina@djangostars.com>
2014-10-12 03:49:24 +0200

    add profile

--- healthpoints/src/healthpoints/static/js/main.js
+++ healthpoints/src/healthpoints/static/js/main.js
@@ -1,11 +1,26 @@
 'use strict';
 
 $(function () {
-  $("#slider-level").slider({
+  /*$("#slider-level").slider({
     value: 0,
     min: 0,
     max: 600,
     step: 100
-  });
+  });*/
 
+ var select = $( "#level-definition" );
+ //$( "#level-definition" ).selectmenu();
+ var slider = $( "#slider-level" ).slider({
+   min: 0,
+   max: 6,
+   step: 1,
+   range: "min",
+   value: select[ 0 ].selectedIndex,
+     slide: function( event, ui ) {
+     select[ 0 ].selectedIndex = ui.value;
+   }
+ });
+ $( "#level-definition" ).change(function() {
+   slider.slider( "value", this.selectedIndex );
+ });
 });
